24f27f6618cf6d2ecd73e3352f572f78a8074ea0
ssmitee <144239782+ssmitee@users.noreply.github.com>
2023-12-23 20:57:43 +0200

    Добавьте файлы проекта.

---
+++ 5-1,Fomichev/Program.cs
@@ -0,0 +1,14 @@
+﻿
+char[] characters = { 'h', 'e', 'l', 'l', 'o', ' ', 'w', 'o', 'r', 'l', 'd', ' ', 't', 'e' };
+string text = new string(characters);
+string[] words = text.Split(' ');
+string longestWord = "";
+    foreach (string word in words)
+    {
+        if (word.Length > longestWord.Length)
+        {
+            longestWord = word;
+        }
+    } 
+Console.WriteLine("Самое длинное слово: " + longestWord);
+Console.WriteLine("Его длина: " + longestWord.Length);
